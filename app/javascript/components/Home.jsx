8143b9f3acb53e147afc7a72eda404810c4c40f0
aarkerio <mmontoya@gmail.com>
2019-11-29 17:12:14 -0600

    New JSON files

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,13 @@
+// app/javascript/components/home.jsx
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <div className="jumbotron">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
+
