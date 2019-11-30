d586e17f6faaf256c36dd5fadae3b77948afdef8
aarkerio <mmontoya@gmail.com>
2019-11-30 13:23:26 -0600

    New API file

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -21,7 +21,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Home />,
     document.body.appendChild(document.createElement('div')),
   )
 })
